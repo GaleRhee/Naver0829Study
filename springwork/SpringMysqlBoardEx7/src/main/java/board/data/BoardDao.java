e76279b0239fbdc4077a6d871aed8f89408ce639
GaleRhee <jun5hero@naver.com>
2023-11-15 11:53:49 +0900

    part1
    111

--- springwork/SpringMysqlBoardEx7/src/main/java/board/data/BoardDao.java
+++ springwork/SpringMysqlBoardEx7/src/main/java/board/data/BoardDao.java
@@ -1,5 +1,7 @@
 package board.data;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -16,6 +18,16 @@ public class BoardDao {
 	public int getTotalCount()
 	{
 		return session.selectOne(nameSpace+"totalCountOfBoard");
+	}
+	
+	public void insertBoard(BoardDto dto)
+	{
+		session.insert(nameSpace+"insertBoard", dto);
+	}
+	
+	public List<BoardDto> getAllDatas()
+	{
+		return session.selectList(nameSpace+"selectAllBoard");
 	}
 	
 	

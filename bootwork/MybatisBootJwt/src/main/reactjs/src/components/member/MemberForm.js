19b64c7d92498355514b82344f5aa18102dae14d
GaleRhee <jun5hero@naver.com>
2024-01-24 19:41:21 +0900

    react day0124 login, member

--- bootwork/MybatisBootJwt/src/main/reactjs/src/components/member/MemberForm.js
+++ bootwork/MybatisBootJwt/src/main/reactjs/src/components/member/MemberForm.js
@@ -2,10 +2,47 @@ import { CameraAltOutlined } from '@mui/icons-material';
 import axios from 'axios';
 import React, { useState } from 'react';
 import errimg from '../../image/noimage.png';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import DaumPostcodeEmbed from 'react-daum-postcode';
+import { useNavigate } from 'react-router-dom';
 
 const MemberForm = () => {
     const [photo, setPhoto] = useState('noimage.png');
     const [addr, setAddr] = useState('');
+    const [open, setOpen] = useState(false); //dialog open/close
+    const [openPostcode, setOpenPostcode] = useState(false); //카카오 주소록 open/close
+    const [idcheck, setIdcheck] = useState(false); //아이디 중복 확인을 했는지 체크하기 위한 변수
+    const [myid, setMyid] = useState('');
+    const [name, setName] = useState('');
+    const [pass, setPass] = useState('');
+    const [hp, setHp] = useState('');
+
+    const navi = useNavigate();
+
+    const handleClickOpen = () => {
+        setOpen(true);
+        setOpenPostcode(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+        setOpenPostcode(false);
+    };
+
+    //주소 선택 완료 시 호출될 이벤트
+    const selectAddress = (data) => {
+        console.dir(data);
+        setAddr(`(${data.zonecode}) ${data.address} ${data.buildingName}`);
+
+        //주소 선택 시 출력 후 카카오 주소록과 dialog 를 닫는다
+        setOpen(false);
+        setOpenPostcode(false);
+    }
 
     //네이버 스토리지의 이미지 폴더명
     const imageUrl = process.env.REACT_APP_STORAGE;
@@ -23,8 +60,85 @@ const MemberForm = () => {
             setPhoto(res.data); //사진 변경 - 스토리지에 업로드된 파일명을 서버가 반환
         })
     }
+
+    //중복 확인 버튼 이벤트
+    const buttonIdCheck = () => {
+        const url = "/member/idcheck?myid=" + myid;
+        axios.get(url)
+            .then(res => {
+                if (Number(res.data) === 0) {
+                    alert("사용 가능한 아이디입니다");
+                    setIdcheck(true);
+                } else {
+                    alert("이미 사용 중인 아이디입니다");
+                    setMyid('');
+                    setIdcheck(false);
+                }
+            })
+    }
+
+    //데이터 저장 이벤트
+    const saveMemberEvent = () => {
+        if (myid.length === 0) {
+            alert("아이디를 입력 후 중복 확인해주세요");
+            return;
+        }
+
+        if (!idcheck) {
+            alert("아이디 중복 확인 버튼을 눌러주세요");
+            return;
+        }
+
+        if (name.length === 0) {
+            alert("이름을 입력해주세요");
+            return;
+        }
+
+        if (pass.length === 0) {
+            alert("비밀번호를 입력해주세요");
+            return;
+        }
+
+        //db 저장
+        axios.post("/member/insert", { name, myid, pass, hp, addr })
+            .then(res => {
+                //멤버 추가 후 어디로 갈지?
+                navi("/member/list");
+            })
+    }
+
     return (
         <div>
+            {/* 카카오 주소록을 보기 위한 dialog */}
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"카카오 주소록"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        {
+                            //카카오 주소 창
+                            openPostcode &&
+                            <DaumPostcodeEmbed
+                                onComplete={selectAddress} //값을 선택할 경우 실행되는 이벤트
+                                autoClose={false} //값을 선택할 경우 사용되는 DOM 을 제거하여 자동 닫힘 설정
+                                defaultQuery='강남대로 24길'//팝업을 열 때 검색창의 기본 주소
+                            />
+                        }
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Disagree</Button>
+                    <Button onClick={handleClose} autoFocus>
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog>
             <h4 className='alert alert-danger'>회원 가입</h4>
             <table style={{ width: '530px' }} className='table table-bordered'>
                 <tbody>
@@ -41,41 +155,52 @@ const MemberForm = () => {
                         </td>
                         <td width={100} style={{ backgroundColor: 'lightgray' }}>이름</td>
                         <td>
-                            <input type='text' className='form-control' />
+                            <input type='text' className='form-control'
+                                value={name} onChange={(e) => setName(e.target.value)} />
                         </td>
                     </tr>
                     <tr>
                         <td width={100} style={{ backgroundColor: 'lightgray' }}>아이디</td>
                         <td className='input-group'>
                             <input type='text' className='form-control'
-                                style={{ width: '120px' }} />
-                            <button type='button' className='btn btn-sm btn-outline-danger'>중복 확인</button>
+                                style={{ width: '120px' }} value={myid}
+                                onChange={(e) => {
+                                    setIdcheck(false); //아이디 입력 시 중복 체크 버튼 다시 눌러야 함
+                                    setMyid(e.target.value);
+                                }} />
+                            <button type='button' className='btn btn-sm btn-outline-danger'
+                                onClick={buttonIdCheck}>중복 확인</button>
                         </td>
                     </tr>
                     <tr>
                         <td width={100} style={{ backgroundColor: 'lightgray' }}>비밀번호</td>
                         <td>
-                            <input type='text' className='form-control' />
+                            <input type='password' className='form-control'
+                                value={pass} onChange={(e) => setPass(e.target.value)} />
                         </td>
                     </tr>
                     <tr>
                         <td width={100} style={{ backgroundColor: 'lightgray' }}>핸드폰</td>
                         <td>
-                            <input type='text' className='form-control' />
+                            <input type='text' className='form-control'
+                                value={hp} onChange={(e) => setHp(e.target.value)} />
                         </td>
                     </tr>
                     <tr>
                         <td colSpan={3}>
                             <div className='input-group'>
                                 <input type='text' className='form-control'
-                                    style={{ width: '400px' }} />
-                                <button type='button' className='btn btn-sm btn-secondary'>주소 검색</button>
+                                    style={{ width: '400px' }} value={addr}
+                                    onChange={(e) => setAddr(e.target.value)} />
+                                <button type='button' className='btn btn-sm btn-secondary'
+                                    onClick={handleClickOpen}>주소 검색</button>
                             </div>
                         </td>
                     </tr>
                     <tr>
                         <td colSpan={3} align='center'>
-                            <button type='button' className='btn btn-outline-success'>저장하기</button>
+                            <button type='button' className='btn btn-outline-success'
+                                onClick={saveMemberEvent}>저장하기</button>
                         </td>
                     </tr>
                 </tbody>
--- bootwork/MybatisBootJwt/src/main/reactjs/src/components/member/MemberList.js
+++ bootwork/MybatisBootJwt/src/main/reactjs/src/components/member/MemberList.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import MemberRowItem from './MemberRowItem';
+
 
 const MemberList = () => {
+    const [memberlist, setMemberlist] = useState([]);
+    //const [totalCount, setTotalCount] = useState(0);
+
+    //const navi = useNavigate();
+
+    //네이버 스토리지의 이미지 폴더명
+    //const imageUrl = process.env.REACT_APP_STORAGE;
+
+    const getMemberlist = () => {
+        axios.get("/member/list")
+            .then(res => {
+                setMemberlist(res.data.mlist);
+                //setTotalCount(res.data.totalCount);
+            })
+    }
+
+    useEffect(() => {
+        getMemberlist();
+    }, []);
+
+    //삭제 이벤트
+    const deleteMember = (num) => {
+        const a = window.confirm("삭제하려면 확인을 눌러주세요");
+        if (a) {
+            const url = "/member/delete?num=" + num;
+            axios.delete(url)
+                .then(res => {
+                    //삭제 성공 후 목록 다시 출력
+                    getMemberlist();
+                })
+        }
+    }
+
     return (
         <div>
-            <h4 className='alert alert-danger'>회원 목록</h4>
+            {/* <h4 className='alert alert-danger'>회원 목록 (총 {totalCount}명)</h4> */}
+            <h4 className='alert alert-danger'>회원 목록 (총 {memberlist.length}명)</h4>
+            <hr />
+            <table className='table table-bordered'>
+                <tbody>
+                    {
+                        memberlist &&
+                        memberlist.map((row, idx) => (
+                            <MemberRowItem key={idx} row={row}
+                                deleteMember={deleteMember} />
+                        ))
+                    }
+                </tbody>
+            </table>
         </div>
     );
 };
